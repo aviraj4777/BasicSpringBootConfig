39bf391e9f2790a6aa09c943e31bffc12bac86bc
aviraj4777 <aviraj4777@gmail.com>
2026-02-24 23:27:05 +0530

    Added BaseMode with MappedSuperClasss

--- src/main/java/com/uber/review/models/Review.java
+++ src/main/java/com/uber/review/models/Review.java
@@ -1,47 +1,25 @@
 package com.uber.review.models;
-
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "booking_reviews")
-@EntityListeners(AuditingEntityListener.class)
-public class Review {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Review extends BaseModel {
     
     @Column(nullable = false)
     private String content;
     
     @Column
     private Double rating;
-    
-    @Column(nullable = false)
-    @CreatedDate
-    private LocalDateTime createdAt;
-    
-    @Column(nullable = false)
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 }
